52980305e9def99654df1a6254ff190917d249b8
Koreneva Natalya <korentusya97@gmail.com>
2023-08-20 13:16:34 +0300

    add value

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,6 +2,7 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String massage = "Hello, world!";
+        System.out.println(massage);
     }
 }
